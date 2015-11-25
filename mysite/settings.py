416756afa67571ca55362269a86d7f7f8e884a3d
jihoon0335 <dla0335@kut.ac.kr>
2015-11-26 01:53:41 +0900

    11-26

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,9 +111,6 @@ LOGIN_REDIRECT_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    
 MEDIA_URL = '/media/'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
